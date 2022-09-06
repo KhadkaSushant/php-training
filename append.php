1721df1a59f0a5cc3d1901a4fadb6dd5dcd03d42
Sushant Khadka <sushantk9869@gmail.com>
2022-09-06 07:35:10 +0545

    prepend & append

---
+++ append.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="append.php" method="post">
+        <textarea name="text"></textarea><br>
+        Prepend:<input type="text" name="prepend"><br>
+        Append:<input type="text" name="append"><br>
+        <button>Summit</button>
+    </form>
+    
+     <pre>
+        <?php var_dump($_POST); ?>
+     </pre>
+        <?php
+
+            if(!empty($_POST)){
+    
+                $text=$_POST['text'];
+                $prepend=$_POST['prepend'];
+                $append=$_POST['append'];
+
+                
+                echo $prepend . $append;
+            }
+        ?>
+    
+</body>
+</html>
