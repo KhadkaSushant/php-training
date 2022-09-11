aea2b15a05997be5ddcb64771f781667773c537c
Sushant Khadka <sushantk9869@gmail.com>
2022-09-11 10:53:05 +0545

    Array

---
+++ array.php
@@ -0,0 +1,40 @@
+ <?php
+    /*$array=[
+        2 => 1,
+        5 => 2,
+        1 => 3,
+        4 => 4,
+        8 => 1
+    ];*/
+    $array=[1,2,3,4,5];
+    echo "<pre>";
+    // print_r($array);
+    // array_push($array,6);
+    // print_r($array);
+    // $array[]=7;
+    // print_r($array);
+
+    // array_pop($array);
+    // print_r($array);
+
+    // array_shift($array);
+    // print_r($array);
+
+    // sort($array);
+    // print_r($array);
+
+    //ksort($array);
+    //print_r($array);
+
+    //rsort($array);
+    //print_r($array);
+
+    //krsort($array);
+    //print_r($array);
+
+    //$array[2]=6;
+    //print_r($array);
+
+    $name = array('sushant'=>'3','suman'=>'8');
+    print_r(array_keys($name));
+?>
